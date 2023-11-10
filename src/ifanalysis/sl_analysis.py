a3d7f5a4b34b022c76a5786ed6f98c4c4a65321d
hh65 <hh65@sussex.ac.uk>
2023-11-10 10:11:08 +0000

    fixed index problems and improved intensity figure in sl_analysis.py::intensity_plot

--- src/ifanalysis/sl_analysis.py
+++ src/ifanalysis/sl_analysis.py
@@ -5,7 +5,7 @@ from matplotlib import ticker
 import seaborn as sns
 from ifanalysis._helper_functions import save_fig
 from pathlib import Path
-
+from typing import Optional
 path = Path.cwd() 
 
 
@@ -48,27 +48,75 @@ def combplot_hist(df, conditions, title_str, cell_number=None, save=True, path=p
     if save:
         save_fig(fig, path, title_str, fig_extension="png")
 
-def plot_int_violin(ax, df: pd.DataFrame, conditions: list, col: str):     
-    sns.violinplot(
-        x="condition", 
-        y=col, 
-        data=df, 
-        order=conditions, 
-        dodge=False, 
-        density_norm='width', 
-        inner=None, 
-        linewidth=0,
-        ax=ax)
+def plot_int_violin(ax, df: pd.DataFrame, conditions: list, col: str, hue: Optional[str]):
+    # Determine the number of hue levels
+
+    if hue:
+        sns.violinplot(
+            x="condition", 
+            y=col, 
+            data=df, 
+            order=conditions,
+            dodge=True, 
+            density_norm='width', 
+            inner=None, 
+            linewidth=0,
+            hue=hue,
+            ax=ax)
+        
+         # Calculate the width of each violin plot portion
+        hue_levels = df[hue].unique()
+        hue_count = len(hue_levels)
+        width_per_hue = 0.8 / hue_count  # 0.8 is the default width of violin plots in seaborn
+
+        for i, condition in enumerate(conditions):
+            for j, level in enumerate(hue_levels):
+                subset = df[(df["condition"] == condition) & (df[hue] == level)]
+                # Calculate the center position for the boxplot
+                box_center = i - 0.4 + width_per_hue/2 + j*width_per_hue
+                sns.boxplot(
+                    y=subset[col], 
+                    showfliers=False, 
+                    color='white', 
+                    saturation=0.5, 
+                    width=0.1, 
+                    boxprops={'zorder': 2},
+                    positions=[box_center], 
+                    ax=ax)
+    else:
+        sns.violinplot(
+            x="condition", 
+            y=col, 
+            data=df, 
+            order=conditions,
+            dodge=False, 
+            density_norm='width', 
+            inner=None, 
+            linewidth=0,
+            ax=ax)
+        sns.boxplot(
+            x="condition", 
+            y=col, 
+            data=df, 
+            order=conditions, 
+            showfliers=False, 
+            color='white', 
+            saturation=0.5, 
+            width=0.2, 
+            boxprops={'zorder': 2},
+            ax=ax)
+
     for violin in ax.collections:
         violin.set_alpha(1)
-    sns.boxplot(x="condition", y=col, data=df, order=conditions, showfliers=False, color='white', saturation=0.5, width=0.2, boxprops={'zorder': 2},ax=ax)
+
     ax.set_xticks(range(len(conditions)))
     ax.set_xticklabels(conditions, rotation=30, ha='right')
     ax.set_ylabel('')
-    ax.set_xlabel('') 
+    ax.set_xlabel('')
 
-def intensity_plot(df, conditions, columns, title=None, save=True, path=path):
-    fig, axarr = plt.subplots(ncols=len(columns), figsize=(3 * len(conditions), 3))
+
+def intensity_plot(df, conditions, columns, hue=None, title=None, save=True, path=path):
+    fig, axarr = plt.subplots(nrows=len(columns), figsize=(len(conditions), len(columns)*3))
     
     # Ensure axarr is always a list
     if len(columns) == 1:
@@ -77,12 +125,12 @@ def intensity_plot(df, conditions, columns, title=None, save=True, path=path):
     for i, col in enumerate(columns):
         y_max = df[col].quantile(0.99) * 1.5
         y_min = df[col].quantile(0.01) * 0.8
-        plot_int_violin(axarr[i], df, conditions, col)
+        plot_int_violin(axarr[i], df, conditions, col, hue)
         axarr[i].set_ylim([y_min, y_max])
         axarr[i].set_title(col)   
 
     fig.suptitle(title, size=14, weight='bold', x=0.01, horizontalalignment='left')
     plt.subplots_adjust(top=0.8)
     if save:
-        save_fig(fig, path, title, tight_layout=False, fig_extension="png")
+        save_fig(fig, path, title, tight_layout=True)
     
